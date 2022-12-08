b8ad9264237da9cd53b1a122b59b11443976e580
Christian <c.espinoza.code@gmail.com>
2022-12-08 12:46:52 -0500

    promises done

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 console.log("Hello My friend")
 
-const API_URL = 'https://dog.ceo/api/breeds/list/all'
+const API_URL = 'https://dog.ceo/api/breeds/image/random'
+
+fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        //data.message
+        console.log(data.message)
+    })
