0d972d2b69049aef5f3900765914eef8f3284157
Christian <c.espinoza.code@gmail.com>
2022-12-08 12:49:12 -0500

    validation added in promise to dog api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const API_URL = 'https://dog.ceo/api/breeds/image/random'
 fetch(API_URL)
     .then(res => res.json())
     .then(data => {
+        const img = document.querySelector('img')
+        if (img != null) {
+            img.src = data.message
+        }
         //data.message
         console.log(data.message)
     })
